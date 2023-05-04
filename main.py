31b1180c0928590f7abd776c502fd670a7f91caf
Prateek <prateek@Prateeks-MacBook-Air.local>
2023-05-03 22:26:15 -0400

    mostly done, left with alreadyPresent function

---
+++ main.py
@@ -0,0 +1,62 @@
+def outOfColumns(row, column):
+    if (column == 8):
+        row += 1
+        column = 0
+    else:
+        column += 1
+    return row, column
+
+
+def alreadyPresent(sudokuBoard, row, column, num):
+    for i in range(0, 9):
+        if (sudokuBoard[row][i] == num):
+            return True
+
+    for i in range(0, 9):
+        if (sudokuBoard[i][column] == num):
+            return True
+
+    return False
+
+
+def sudokuSolver(sudokuBoard, row, column):
+    if row == 9:  # if at the end of the board, no more backtracking
+        return True
+
+    rowUpdated, columnUpdated = outOfColumns(row, column)
+
+    if (sudokuBoard[row][column] > 0):
+        return sudokuSolver(sudokuBoard, rowUpdated, columnUpdated)
+
+    for i in range(1, 10):
+        if not (alreadyPresent(sudokuBoard, row, column, i)):
+            sudokuBoard[row][column] = i
+            if sudokuSolver(sudokuBoard, rowUpdated, columnUpdated):
+                return True
+        sudokuBoard[row][column] = 0
+
+    return False
+
+
+def printSudoku(sudokuBoard):
+    for i in range(0, 9):
+        for j in range(0, 9):
+            print(sudokuBoard[i][j], end=" ")
+        print()
+    print()
+
+
+if __name__ == "__main__":
+    sudokuBoard = [[0, 0, 0, 3, 1, 0, 6, 0, 0],
+                   [6, 0, 0, 2, 0, 0, 1, 0, 0],
+                   [0, 8, 0, 0, 5, 0, 2, 0, 0],
+                   [0, 5, 4, 0, 0, 0, 0, 0, 0],
+                   [9, 7, 0, 0, 0, 0, 0, 8, 3],
+                   [0, 0, 0, 0, 0, 0, 7, 4, 0],
+                   [0, 0, 3, 0, 2, 0, 0, 6, 0],
+                   [0, 0, 8, 0, 0, 4, 0, 0, 7],
+                   [0, 0, 2, 0, 3, 7, 0, 0, 0]]
+
+    printSudoku(sudokuBoard)
+    sudokuSolver(sudokuBoard, 0, 0)
+    printSudoku(sudokuBoard)
